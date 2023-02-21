b9ac3d59c759dbbab59eae554286944c11b7b2c7
quick6response <romanov201573@mail.ru>
2023-02-21 14:44:41 +0300

    Новые компоненты для отрисовки трех-уровневой вложенной структуру списка

---
+++ src/components/screens/Home/Drawer/DrawerListItem.tsx
@@ -0,0 +1,128 @@
+import {
+  Divider,
+  Drawer,
+  IconButton,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import List from "@mui/material/List";
+import React, { FC } from "react";
+import { ItemInterface } from "../interfaces/item.interface";
+import {
+  FullListLayoutsInterface,
+  GroupLayoutsType,
+} from "../interfaces/layouts.interface";
+import { ListGroupsItemsLayouts } from "../List/ListGroupsItemsLayouts";
+
+export const drawerWidth = 310;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(1),
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  popup: {
+    padding: theme.spacing(0),
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerHeader: {
+    padding: theme.spacing(0, 1),
+    height: "10px",
+    alignItems: "flex-end",
+  },
+  list: {
+    width: 300,
+    marginLeft: "auto",
+    overflowX: "hidden",
+    marginTop: "100px",
+    paddingTop: "65px",
+  },
+}));
+
+interface IAddList {
+  isOpen: boolean;
+  // выбранные элементы
+  items: ItemInterface[];
+  //доступные элементы
+  originalItems: FullListLayoutsInterface[];
+
+  changeIsOpen: () => void;
+  onAddItem: (group: GroupLayoutsType, itemId: string) => void;
+  onRemoveItem: (itemId: string) => void;
+}
+
+export const DrawerListAddElement: FC<IAddList> = ({
+  items,
+  onRemoveItem,
+  onAddItem,
+  originalItems,
+  isOpen,
+  changeIsOpen,
+}) => {
+  const classes = useStyles();
+
+  /**
+   * Добавить/удалить элемент
+   * @param checked Выбран ли этот элемент
+   * @param group
+   * @param itemId
+   */
+  const handleChange = (
+    checked: boolean,
+    group: GroupLayoutsType,
+    itemId: string
+  ) => {
+    if (checked) onAddItem(group, itemId);
+    else onRemoveItem(itemId);
+  };
+
+  return (
+    <>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={isOpen}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={() => changeIsOpen()}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List className={classes.list}>
+          <ListItem
+            key="menuHeading"
+            divider
+            disableGutters
+            style={{ paddingTop: "65px" }}
+          >
+            <ListItemText inset primary="Доступные виджеты" />
+          </ListItem>
+
+          {originalItems.map((layouts) => (
+            <div style={{ marginLeft: 7 }}>
+              <ListGroupsItemsLayouts
+                key={layouts.name}
+                items={items.filter((i) => i.group === layouts.data.group)}
+                elementLayouts={layouts}
+                handleChange={handleChange}
+              />
+            </div>
+          ))}
+        </List>
+      </Drawer>
+    </>
+  );
+};
---
+++ src/components/screens/Home/List/ListGroupsItemsLayouts.tsx
@@ -0,0 +1,51 @@
+import { Collapse, List, ListItemText } from "@material-ui/core";
+import { ExpandLess } from "@material-ui/icons";
+import { ExpandMore } from "@mui/icons-material";
+import ListItemButton from "@mui/material/ListItemButton";
+import React, { FC, useState } from "react";
+import { ItemInterface } from "../interfaces/item.interface";
+import {
+  FullListLayoutsInterface,
+  GroupLayoutsType,
+} from "../interfaces/layouts.interface";
+import { ListItemsLayouts } from "./ListItemsLayouts";
+
+interface IListItemsLayouts {
+  // выбранные элементы в текущей категории
+  items: ItemInterface[];
+  // объект с категорией
+  elementLayouts: FullListLayoutsInterface;
+  handleChange: (
+    checked: boolean,
+    group: GroupLayoutsType,
+    itemId: string
+  ) => void;
+}
+
+export const ListGroupsItemsLayouts: FC<IListItemsLayouts> = ({
+  items,
+  elementLayouts,
+  handleChange,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <ListItemButton onClick={() => setIsOpen((prevState) => !prevState)}>
+        <ListItemText primary={elementLayouts?.name} />
+        {isOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+
+      <Collapse in={isOpen} timeout="auto" unmountOnExit>
+        <List style={{ marginLeft: 7 }}>
+          <ListItemsLayouts
+            key={elementLayouts.name}
+            items={items.filter((i) => i.group === elementLayouts.data.group)}
+            elementLayouts={elementLayouts?.data}
+            handleChange={handleChange}
+          />
+        </List>
+      </Collapse>
+    </>
+  );
+};
